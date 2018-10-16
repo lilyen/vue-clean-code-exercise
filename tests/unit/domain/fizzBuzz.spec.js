e03a73e4f9edacf5829b5395920068c9ebf4f283
lily <lily.yen79@TI03BY60>
2018-10-16 11:55:55 +0800

    unit test function

---
+++ tests/unit/domain/fizzBuzz.spec.js
@@ -0,0 +1,25 @@
+//import fizzBuzz from '@/domain/fizzBuzz.vue'
+function passNum(num) {
+    if (num % 3 != 0 && num % 5 != 0) {
+        return num.toString();
+    }
+
+    var str = "";
+    if (num % 3 == 0) {
+        str += "fizz";
+    }
+    if (num % 5 == 0) {
+        str += "buzz";
+    }
+    return str;
+}
+
+
+describe('fizzBuzzGame', () => {
+    it('should return 1', () => {
+        expect(passNum(1)).toEqual("1")
+    })
+    it('3 should return fizz', () => {
+        expect(passNum(3)).toEqual("fizz")
+    })
+});
