ef78220ae8784c7ecd818e46e10ba1a6554ca508
lily <lily.yen79@TI03BY60>
2018-10-16 14:26:54 +0800

    unit test haveBeenCall

---
+++ tests/unit/components/profile.spec.js
@@ -0,0 +1,44 @@
+import profile from "@/views/Profile"
+import Api from '@/api'
+import { shallowMount } from '@vue/test-utils'
+
+describe("profile", () => {
+    it("save call api updateProfile", () => {
+        let updateProfile = Api.updateProfile = jest.fn();
+        let wrapper = shallowMount(profile);
+
+        wrapper.vm.save();
+
+        expect(updateProfile).toHaveBeenCalled();
+    })
+    //it("save call api updateProfile meth2", () => {
+    //    let wrapper = shallowMount(profile);
+
+    //    wrapper.vm.save();
+
+    //    expect(Api.updateProfile).toHaveBeenCalled();
+    //})
+    it("save call updateProfile use profile data", () => {
+        let updateProfile = Api.updateProfile = jest.fn();
+        let wrapper = shallowMount(profile);
+        wrapper.vm.profile = {
+            name: 'Lily',
+            birthday: {
+                year: 1993,
+                month: 2,
+                day: 1,
+            }
+        };
+
+        wrapper.vm.save();
+
+        expect(updateProfile).toHaveBeenCalledWith({
+            name: 'Lily',
+            birthday: {
+                year: 1993,
+                month: 2,
+                day: 1,
+            }
+        });
+    })
+})
